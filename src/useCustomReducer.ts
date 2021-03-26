d31c8a996a320307cdc830d39c0bc0fef06cdfc4
Mukul Jain <jainmukul1996@gmail.com>
2021-03-26 08:39:53 +0530

    feat: improved types

--- src/useCustomReducer.ts
+++ src/useCustomReducer.ts
@@ -1,39 +1,31 @@
-import * as React from "react";
+import * as React from 'react';
 
-type ActionCretor<S, A> = Record<keyof A, (...payload: any) => (s: S) => S>;
-type ReducerAction<R extends ActionCretor<any, any>> = R extends ActionCretor<
-  any,
-  infer A
->
-  ? A
-  : never;
-type ReducerState<R extends ActionCretor<any, any>> = R extends ActionCretor<
+type ActionCreator<S, A> = Record<keyof A, (...payload: any) => (s: S) => S>;
+type ReducerState<R extends ActionCreator<any, any>> = R extends ActionCreator<
   infer S,
   any
 >
   ? S
   : never;
 
-export type IActions<S> = Record<string, (...payload: any) => (state: S) => S>;
-
-const useCustomReducer = <R extends ActionCretor<any, any>>(
+const useCustomReducer = <R extends ActionCreator<any, any>>(
   actions: R,
-  initialState: ReducerState<R>
+  initialState: ReducerState<R>,
 ): [ReducerState<R>, R] => {
   const reducer = (
-    state: ReducerState<R>,
-    action: { type: string; payload?: any }
+    s: ReducerState<R>,
+    action: { type: string; payload?: any },
   ) => {
     if (!actions[action.type]) {
       throw new Error();
     }
 
-    return actions[action.type](...action.payload)(state);
+    return actions[action.type](...action.payload)(s);
   };
   const [state, dispatch] = React.useReducer(reducer, initialState);
 
   const newActions: any = {};
-  Object.keys(actions).forEach((actionName) => {
+  Object.keys(actions).forEach(actionName => {
     newActions[actionName] = (...payload: any) =>
       dispatch({ type: actionName, payload });
   });
